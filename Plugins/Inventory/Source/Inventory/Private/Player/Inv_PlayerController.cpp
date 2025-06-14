2df0d34e83f4c70165d5996d11407720fdc821a4
Antares <elgranpalicron@gmail.com>
2025-06-14 12:10:11 -0500

    Class 6

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/Inv_PlayerController.h"
+#include "Inventory.h"
+
+void AInv_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogInventory, Log, TEXT("BeginPlay For PlayerController"));
+}
