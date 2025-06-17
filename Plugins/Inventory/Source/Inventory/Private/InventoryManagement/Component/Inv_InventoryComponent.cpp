f24d2df70a0a9826aafe0723d1a023ee263a2b80
Antares <elgranpalicron@gmail.com>
2025-06-16 22:52:41 -0500

    class 13

---
+++ Plugins/Inventory/Source/Inventory/Private/InventoryManagement/Component/Inv_InventoryComponent.cpp
@@ -0,0 +1,35 @@
+﻿
+#include "InventoryManagement/Component/Inv_InventoryComponent.h"
+#include "Inventory/Public/Widgets/Inventory/InventoryBase/Inv_InventoryBase.h"
+#include "Blueprint/UserWidget.h"
+
+UInv_InventoryComponent::UInv_InventoryComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+// Called when the game starts
+void UInv_InventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	ConstructInventory();
+}
+
+void UInv_InventoryComponent::ConstructInventory()
+{
+	OwningController = Cast<APlayerController>(GetOwner());
+	checkf(OwningController.IsValid(),TEXT("Inventory Component should have a Player Controller As Owner "));
+
+	if (!OwningController->IsLocalController())
+	{
+		return;
+	}
+
+	InventoryMenu = CreateWidget<UInv_InventoryBase>(OwningController.Get(), InventoryMenuClass);
+	InventoryMenu->AddToViewport();
+}
+
+
+
---
+++ Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Component/Inv_InventoryComponent.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Inv_InventoryComponent.generated.h"
+
+
+class UInv_InventoryBase;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable)
+class INVENTORY_API UInv_InventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UInv_InventoryComponent();
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Inventory")
+	TSubclassOf<UInv_InventoryBase> InventoryMenuClass;
+
+private:
+
+	TWeakObjectPtr<APlayerController> OwningController;
+	
+	UPROPERTY()
+	TObjectPtr<UInv_InventoryBase> InventoryMenu;
+	
+	void ConstructInventory();
+
+	
+};
