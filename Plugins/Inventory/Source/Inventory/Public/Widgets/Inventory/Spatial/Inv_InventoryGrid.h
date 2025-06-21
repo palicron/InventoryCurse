458c35e6ab0d42a0a6468c1de994849e85c56430
Antares <elgranpalicron@gmail.com>
2025-06-21 12:18:38 -0500

    class 18

---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_InventoryGrid.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Types/Inv_GridTypes.h"
+#include "Inv_InventoryGrid.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UInv_InventoryGrid : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category="Inventory")
+	EInv_ItemCategory GetItemCategory() const { return ItemCategory; }
+	
+protected:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Inventory")
+	EInv_ItemCategory ItemCategory;
+};
